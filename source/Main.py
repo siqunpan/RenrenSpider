cbba685477ea13d676cff80a6091c7df757f89d9
pansiqun <pansiqun@gmail.com>
2018-06-11 21:53:14 +0800

    add some comments

--- source/Main.py
+++ source/Main.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
 '''
-    This project works for spidering and download info from www.renren.com include:
-    1. personal info
-    2. photos
-    3. blogs
-    4. status
-    5. friends info
-    6. comments
-    7. gossip (old renren version)
-    8. chat (new renren version)
-    9. save your own and all your friends personal info into a database(mysql)
+    这个project主要用来从www.renren.com上爬取并下载如下信息:
+    1. 相册照片及评论album and photo
+    2. 日志及评论blog
+    3. 状态及评论status
+    4. 留言板功能gossip(旧版人人，新版保留了)
+    5. 聊天功能chat(新版人人)
+    6. 自己以及所有好友的信息，出现存储到mysql数据库中，再存储到本地
 '''
 
 from Spider import RenrenSpider
@@ -39,26 +36,32 @@ def Main():
     baobao.login()
     userID = baobao.getUserID()
 
-    CreatePaths(userID)  #userID of url opened
-    CreatePaths(ownerID = PrivateConfig.OwnerID)  #userID of your own
+    CreatePaths(userID)  #在打开的URL中能使用的ID
+    CreatePaths(ownerID = PrivateConfig.OwnerID)  #自己的ID
     
     print ('userID and ownerID: ', userID, ', ', PrivateConfig.OwnerID)
 
+    #相册照片及评论album and photo
     albumManager = AlbumManager(baobao, userID, PrivateConfig.OwnerID)
     albumManager.work()
 
+    #日志及评论blog
     blogManager = BlogManager(baobao, userID, PrivateConfig.OwnerID)
     blogManager.work()
 
+    #状态及评论status
     status = Status(baobao, userID, PrivateConfig.OwnerID)
     status.work()
 
+    #留言板功能gossip(旧版人人，新版保留了)
     gossip = Gossip(baobao, userID, PrivateConfig.OwnerID)
     gossip.work()
 
+    #聊天功能chat(新版人人)
     chatManager = ChatManager(baobao, userID, PrivateConfig.OwnerID)
     chatManager.work()
 
+    #自己以及所有好友的信息，出现存储到mysql数据库中，再存储到本地
     repo = RepoMysql(baobao, userID)
     repo.work()
 
